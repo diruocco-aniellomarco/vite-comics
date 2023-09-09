74de04f9283ed3140c13aad73813ff22a5b30434
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-09-09 12:09:00 +0200

    Setup Web Style

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Ciao",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss">
+h1 {
+  color: red;
+}
+</style>
